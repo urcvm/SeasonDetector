78d2dc2832a7d69dc6953ebe66ccd8c1f11ba982
Shivam Verma <shivam.verma03@ad.infosys.com>
2020-09-25 08:43:59 +0530

    Final Comit

---
+++ src/Season.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+export default class Season extends Component {
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error:{this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <Spinner message="Please accept location request" />;
+  }
+}
---
+++ src/Spinner.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Spinner({ message }) {
+  return (
+    <div className="ui active dimmer">
+      <div className="ui text loader">{message}</div>
+    </div>
+  );
+
+  Spinner.defaultProps = {
+    message: "Loading",
+  };
+}
+
+export default Spinner;
